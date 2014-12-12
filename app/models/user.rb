127399885232f82fb62016c8b86d4dfce05ffc4b
Nirag Amin & Nodira Berdieva <pair+namin2.nberdieva@devbootcamp.com>
2014-12-12 17:04:32 -0600

    User Associations Adding user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :password, presence: true
 
+  has_many :rounds
+  has_many :guesses
+
   def authenticate(password)
     if self.password == (password)
       true
