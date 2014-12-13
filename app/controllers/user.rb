5fc1d27d6df942ad6e16a40f5340cb45cef036dc
Nirag Amin <nirag.amin@gmail.com>
2014-12-13 14:17:27 -0600

    Update user.rb

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -2,27 +2,28 @@ get '/' do
   erb :'index'
 end
 
-get '/session/new' do
+get '/login' do
   erb :'login/index'
 end
 
-post '/session' do
+post '/login' do
   @user = User.find_by(name: params[:user][:name])
   if @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
     redirect '/decks'
   else
-    redirect '/login/new'
+    redirect '/login'
   end
 end
 
-get '/users/new' do
-  erb :'/users/new'
+get '/login/new' do
+  erb :'/login/new'
 end
 
-post '/users' do
+post '/login/new' do
   @user = User.new(params[:user])
   if @user.save
+    erb :'index'
     redirect '/decks'
   else
     redirect '/login/new'
