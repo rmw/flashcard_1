a89b6e2071af7a3060eb08d95f873b29470c396b
Nirag Amin & Nodira Berdieva <pair+namin2.nberdieva@devbootcamp.com>
2014-12-14 00:57:17 -0600

    AWWWW yisss game logic coming along

--- app/controllers/see_deck.rb
+++ app/controllers/see_deck.rb
@@ -7,34 +7,37 @@ get '/decks' do
   end
 end
 
+get '/decks/:deck_id/cards/:index_id' do
+  # card_arr = []
+  @index = params[:index_id]
+  @deck = Deck.find(params[:deck_id])
+  # @round = Round.create(deck_id: @deck.id)
+  @card = @deck.cards[params[:index_id].to_i]
+  # card_arr << @card
+  erb :'/decks/playtoy'
+end
+
 get '/decks/:id' do
   if session[:user_id]  != nil
-    @decks = Deck.find(params[:id])
-    @cards = @decks.cards
+    @deck = Deck.find(params[:id])
+    @cards = @deck.cards
     erb :'decks/show'
   else
     redirect '/login'
   end
 end
 
-post '/answer/:id' do
-  if session[:user_id]  != nil
-    @deck = Deck.find(params[:id])
-    @deck.right_wrong(params[:user_answer])
-    redirect "/decks/play/#{@deck.id}"
-  else
-    redirect '/login'
-  end
-end
+# post '/answer/:id' do
+#   if session[:user_id]  != nil
+#     @deck = Deck.find(params[:id])
+#     @deck.right_wrong(params[:user_answer])
+#     redirect "/decks/play/#{@deck.id}"
+#   else
+#     redirect '/login'
+#   end
+# end
 
-get '/decks/:id/round' do
-  # card_arr = []
-  @deck = Deck.find(params[:id])
-  @round = Round.create(deck_id: @deck.id)
-  @card = @deck.cards.sample
-  # card_arr << @card
-  erb :'/decks/playtoy'
-end
+
 
 get '/round/:id/correct' do
   @round = Round.find(params[:id])
@@ -42,23 +45,21 @@ get '/round/:id/correct' do
   erb :'/decks/correct'
 end
 
-post '/decks/round/:round_id' do
-  @round = Round.find(params[:round_id])
+post '/decks/:deck_id/cards/:index_id' do
+  @user = User.find(session[:user_id])
+  @round = Round.create(deck_id: params[:deck_id], user_id: @user.id)
   @deck = @round.deck
-  @card = @deck.cards.first
+  @card = @deck.cards[params[:index_id].to_i]
   if params[:answer] == @card.answer
     @guess = Guess.create(correct: true, card_id: @card.id)
     # add to 1 to the card index
-    redirect "/round/#{@round.id}/correct"
+    redirect "/decks/#{@deck.id}/cards/#{params[:index_id].to_i + 1}"
   else
-    redirect "/decks/#{@deck.id}/round" # add card index num to url
+    redirect "/decks/#{@deck.id}/cards/#{params[:index_id].to_i}" # add card index num to url
   end
   # /decks/round
 # @deck.cards[params[:index]]
 #
-
-
-
   # redirect to round results page when card index +1 = to the length
   # of cards array
 
@@ -66,6 +67,8 @@ post '/decks/round/:round_id' do
   # @card = @deck.cards.to_a.shift
   # @guess = Guess.create(answer: params[:answer])
 end
+
+
 
 # original play route
 # get '/decks/:id/round' do
@@ -89,4 +92,6 @@ end
 
 #   erb :'/decks/toy'
 # end
+
+
 
