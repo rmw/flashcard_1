4169e127476aa6447789f04075045982cc2845bc
Nirag Amin & Nodira Berdieva <pair+namin2.nberdieva@devbootcamp.com>
2014-12-15 10:11:59 -0600

    Adding css

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,18 @@ require 'faker'
 Deck.create!(name: "World Capitals", description: "Learn the capital for each country in the world.")
 
 
-Card.create(deck_id: 1, question: "Afghanistan" , answer: "Kabul" )
-Card.create(deck_id: 1, question: "Albania" , answer: "Tirana" )
-Card.create(deck_id: 1, question: "Algeria", answer: "Algiers" )
-Card.create(deck_id: 1, question: "Andorra", answer: "Andorra la Vella" )
-Card.create(deck_id: 1, question: "Angola", answer: "Luanda" )
-Card.create(deck_id: 1, question: "Antigua and Barbuda", answer: "St. Johns")
+Card.create(deck_id: 1, question: "China" , answer: "Beijing" )
+Card.create(deck_id: 1, question: "India" , answer: "New Delhi" )
+Card.create(deck_id: 1, question: "Japan", answer: "Tokyo" )
+Card.create(deck_id: 1, question: "Russia", answer: "Moscow" )
+Card.create(deck_id: 1, question: "South Korea", answer: "Seoul" )
+Card.create(deck_id: 1, question: "Tajikistan", answer: "Dushanbe")
 
 
 User.create(name: "Nodira", password: "corgi")
 User.create(name: "Nirag", password:"taco")
+User.create(name: "Armando", password:"pretzelburrito")
+User.create(name: "Vianney", password:"insectmaster")
 
 
 
